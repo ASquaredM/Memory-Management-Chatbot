70b2af8c9d661da606829df1746de0e0f228ac73
ASquaredM <ASquaredM@github.com>
2020-06-29 11:01:36 +0200

    Initial commit

---
+++ src/graphnode.cpp
@@ -0,0 +1,56 @@
+#include "graphedge.h"
+#include "graphnode.h"
+
+GraphNode::GraphNode(int id)
+{
+    _id = id;
+}
+
+GraphNode::~GraphNode()
+{
+    
+
+    delete _chatBot; 
+
+    ////
+    
+}
+
+void GraphNode::AddToken(std::string token)
+{
+    _answers.push_back(token);
+}
+
+void GraphNode::AddEdgeToParentNode(GraphEdge *edge)
+{
+    _parentEdges.push_back(edge);
+}
+
+void GraphNode::AddEdgeToChildNode(GraphEdge *edge)
+{
+    _childEdges.push_back(edge);
+}
+
+
+void GraphNode::MoveChatbotHere(ChatBot *chatbot)
+{
+    _chatBot = chatbot;
+    _chatBot->SetCurrentNode(this);
+}
+
+void GraphNode::MoveChatbotToNewNode(GraphNode *newNode)
+{
+    newNode->MoveChatbotHere(_chatBot);
+    _chatBot = nullptr; // invalidate pointer at source
+}
+
+
+GraphEdge *GraphNode::GetChildEdgeAtIndex(int index)
+{
+    
+
+    return _childEdges[index];
+
+    ////
+    
+}
